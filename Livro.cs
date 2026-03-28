7eb4f16d58b3a32bd63a869a3f354c8d6d265ef1
Hiago 7k <hiago7k@gmail.com>
2026-03-28 12:50:38 -0300

    tentando resolver o problema, de desserealização do objeto

---
+++ Livro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2;
+
+internal class Livro
+{
+
+    [JsonPropertyName("titulo")]
+    public string? Titulo { get; }
+    [JsonPropertyName("autor")]
+    public string? Autor { get; }
+    [JsonPropertyName("ano_publicacao")]
+    public string? AnoPublicacao { get; set; }
+    [JsonPropertyName("genero")]
+    public string? Genero { get; }
+    [JsonPropertyName("paginas")]
+    public int Paginas { get; }
+    [JsonPropertyName("editora")]
+    public string? Editora { get; }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
-﻿using (HttpClient httpClient = new HttpClient())
+﻿using ConsoleApp2;
+using System.Text.Json;
+
+using (HttpClient httpClient = new HttpClient())
 {
-    string resposta = await httpClient.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/Livros.json");
-    Console.WriteLine(resposta);
+
+    try {
+        string resposta = await httpClient.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/Livros.json");
+        var livros = JsonSerializer.Deserialize<List<Livros>>(resposta)!;
+        var livrosPorNome = livros.OrderBy(livros => livros.ListaDeLivros.).ToList()!;
+
+        foreach (var livrosOrdenados in livrosPorNome) 
+        {
+            Console.WriteLine(livrosOrdenados);
+        }
 
 
+    }
+    catch (Exception ex) 
+    {
+        Console.WriteLine($"Erro  {ex:Message}");
+    }
 }

ae28aab68978607dd268d2f192fceff0832e6620
Hiago 7k <hiago7k@gmail.com>
2026-03-27 23:19:08 -0300

    req

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using (HttpClient httpClient = new HttpClient())
+{
+    string resposta = await httpClient.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/Livros.json");
+    Console.WriteLine(resposta);
+
+
+}
